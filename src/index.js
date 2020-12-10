b23ec0f8831e40a820f8657a90effa0c7b815c0f
Tanamix22 <tanamix22@gmail.com>
2020-12-10 17:50:43 -0500

    creacion de avatar

--- src/index.js
+++ src/index.js
@@ -6,14 +6,30 @@ container .appendChild(element) */
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const us = {
+const user = {
   firsName: 'Freenass',
   lastName: 'Backup',
-  avatar:
+  avatar: 'http://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
 }
 
-const program = 'Frenass'
-const element = <h1>Backup, {program} </h1>
+function getName(user){
+  return  `${user.firsName} ${user.lastName}`
+}
+
+function getGreeting(user){
+  if(user){
+    return <h1>program, {getName(user)} </h1>
+  } 
+  return <h1> not Freenass </h1>
+}
+
+const name = 'Frenass'
+const element = (
+  <div>
+    <h1>{getGreeting(user)} </h1>
+    <img src={user.avatar} />
+  </div>
+)
 
 const container = document.getElementById('root')
 
