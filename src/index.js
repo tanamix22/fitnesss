c67ab4c1cd106f8c1a9cc0ad1313079701e0162b
Tanamix22 <tanamix22@gmail.com>
2020-12-15 18:08:18 -0500

    creacion de props

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const user = {
-  firsName: 'Freenass',
-  lastName: 'Backup',
-  avatar: 'http://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
-}
-
-function getName(user){
-  return  `${user.firsName} ${user.lastName}`
-}
-
-function getGreeting(user){
-  if(user){
-    return <h1>program, {getName(user)} </h1>
-  } 
-  return <h1> not Freenass </h1>
-}
-
-const name = 'Frenass'
-const element = (
-  <div>
-    <h1>{getGreeting(user)} </h1>
-    <img src={user.avatar} />
-  </div>
-)
+import Card from './components/card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-
-ReactDOM.render(element, container)
+ReactDOM.render(<Card 
+              title="Technique Guides 2"
+              description="Learn amazing street workout and calisthenics"
+              img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+              lefColor="#A74CF2"
+              rightColor="#617BFB"
+            />, container)
 
